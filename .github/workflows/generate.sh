8be1748ce58c4ad65e4e15edd63f82048457e129
Kenichi Maehashi <939877+kmaehashi@users.noreply.github.com>
2026-04-06 11:44:09 +0900

    Retry tool execution
    Refactor run_with_retry function to handle tool execution more efficiently.

--- .github/workflows/generate.sh
+++ .github/workflows/generate.sh
@@ -5,17 +5,24 @@ cp -a _skeleton _generate
 
 TOOL="discourse_search_atom.py"
 
-"${TOOL}" \
+run_with_retry() {
+  for ((I=0; I<5; I++)); do
+    "$@" && return
+    sleep 10
+  done
+}
+
+run_with_retry "${TOOL}" \
     --name 'PyTorch Forums (cupy)' \
     --url 'https://discuss.pytorch.org' \
     --query 'cupy order:latest' \
     --output '_generate/pytorch-cupy.atom'
-"${TOOL}" \
+run_with_retry "${TOOL}" \
     --name 'Python Community Forums (cupy)' \
     --url 'https://discuss.python.org' \
     --query 'cupy order:latest' \
     --output '_generate/python-cupy.atom'
-"${TOOL}" \
+run_with_retry "${TOOL}" \
     --name 'NVIDIA Developer Forums (cupy)' \
     --url 'https://forums.developer.nvidia.com' \
     --query 'cupy order:latest' \
